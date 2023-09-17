d8ec693178d1566e27a16f2c39736bb082213868
Priya0333 <132730706+Priya0333@users.noreply.github.com>
2023-09-17 08:19:57 +0000

    created home

---
+++ src/main/java/org/jsp/shoppingkart/controller/MainController.java
@@ -0,0 +1,15 @@
+package org.jsp.shoppingkart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String loadHome()
+    {
+        return "Home";
+    }
+    
+    
+}
